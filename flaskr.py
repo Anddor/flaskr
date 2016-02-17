53f186c072217a88d7518201b71bc73a4cf22b37
Andreas Dørum <andreas.doerum@gmail.com>
2016-02-17 14:19:46 +0100

    add templates, bugtest in .py

--- flaskr.py
+++ flaskr.py
@@ -29,7 +29,7 @@ def init_db():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
 
 @app.before_request
@@ -46,17 +46,43 @@ def teardown_request(exception):
 
 @app.route('/')
 def show_entries():
-    cur = g.db.execute('select title, text from entries order by id desc')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    return render_template('show_entries.html', entries=entries)
+    print("trace")
+    # cur = g.db.execute("select title, text from entries order by id desc")
+    # entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    print("showing entries!")
+    return "Hello!"
+    # return render_template("show_entries.html", entries=entries)
 
 
-@app.route('/add', methods['POST'])
+@app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
-    g.db.execute('insert into entries (title, text) values (?, ?)',
+    g.db.execute("insert into entries (title, text) values (?, ?)",
                  [request.form['title'], request.form['text']])
     g.db.commit()
     flash('New entry was successfully posted!')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    print("login")
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+        return redirect('login.html', error=error)
+
+
+@app.route('7logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
     return redirect(url_for('show_entries'))
